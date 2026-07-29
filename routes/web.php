eb3d605b2a1e86a0d63d871b00c016472e51e44f
ardan setiawan <143977930+ardan182@users.noreply.github.com>
2026-07-29 09:59:19 +0700

    Redirect / ke /admin

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Models\TvBoardSetting;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 Route::get('/suppliers/template', function () {
