4b1772880cf3c7cb9792cc1abbbdd9130bff0536
ardan setiawan <143977930+ardan182@users.noreply.github.com>
2026-07-31 10:01:25 +0700

    Fix session: revert bootstrap hack, hardcode session config di config/session.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,10 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
             Request::HEADER_X_FORWARDED_AWS_ELB
         );
 
-        config()->set('session.encrypt', false);
-        config()->set('session.secure', false);
-        config()->set('session.driver', 'database');
-
         $middleware->encryptCookies(except: [
             'jobdeskgudangap-session',
         ]);
